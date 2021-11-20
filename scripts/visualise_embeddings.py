cdc608de06c0e1be43b89ddd450ca131888e90cb
ievapudz <pudziuvelyteieva@gmail.com>
2021-11-20 17:12:01 +0200

    Added visualise_MDE function.

--- scripts/visualise_embeddings.py
+++ scripts/visualise_embeddings.py
@@ -2,9 +2,10 @@ import esm
 import torch
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib import colors
 import numpy
+import pymde
 from sklearn.decomposition import PCA
+from matplotlib import colors
 
 EMB_LAYER = 33
 
@@ -38,3 +39,26 @@ def visualise_PCA(data, keys, plotpath):
     plt.colorbar(sc, label='Variant Effect', ticks=numpy.linspace(37, 80, 2))
     plt.savefig(plotpath)
 
+def visualise_MDE(data, keys, plotpath):
+    # data - dictionary that was created by filter_sequences function.
+    # keys - array of the sets that need to be visualised in one plot.
+    # plotpath - path to the output plot.
+    Ys = []
+    Xs = []
+
+    for key in keys:
+        FASTA_PATH = data[key]['FASTA_prefix']+'sequences.fasta'
+        EMB_PATH = data[key]['embeddings']
+        for header, _seq in esm.data.read_fasta(FASTA_PATH):
+            temperature_label = header.split('|')[-1]
+            Ys.append(int(temperature_label))
+            file_name = header.split('|')[0][1:]
+            fn = f'{EMB_PATH}/{file_name}.pt'
+            embs = torch.load(fn)
+            Xs.append(embs['mean_representations'][EMB_LAYER])
+
+    Xs = torch.stack(Xs, dim=0).numpy()
+    Xs_torch = None
+    Xs_torch = torch.from_numpy(Xs)
+    embedding = pymde.preserve_neighbors(Xs_torch).embed(verbose=True)
+    pymde.plot(embedding, color_by=Ys, savepath=plotpath)
