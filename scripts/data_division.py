f23ad76daa75b0ea69df205720118e06cf2e50c6
ievapudz <pudziuvelyteieva@gmail.com>
2021-11-20 16:18:29 +0200

    Made a separate chunk of code of data division module.

--- scripts/data_division.py
+++ scripts/data_division.py
@@ -1,32 +1,56 @@
 import sys
 from Bio import SeqIO
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
+"""
+Requirements:
+* biopython
+* data.zip file that contains default proteomes
+"""
+
+# Configuration
 temperature_labels_full = [37, 80]
+files = []
+
+if(sys.argv[1] == '-f'):
+    # If the script is run in Colab environment
+    files = ['data/proteomes/UP000000625_83333.fasta', 'data/proteomes/UP000001974_273057.fasta']
+elif(sys.argv == ''):
+    # If the script is run as a part of UNIX pipeline, but no arguments were provided
+    files = ['data/proteomes/UP000000625_83333.fasta', 'data/proteomes/UP000001974_273057.fasta']
+else:
+    # If the script is run as a part of UNIX pipeline with arguments
+    files = sys.argv[1:]
 
 # Initialisation of the dataset container
 data = {
     'train': {
         'X' : [],
         'Y' : [],
-        'prefix': 'training_'
+        'FASTA_prefix': 'data/FASTA/training_',
+        'CSV_prefix': 'data/CSV/training_',
     },
     'validate': {
         'X' : [],
         'Y' : [],
-        'prefix': 'validation_'
+        'FASTA_prefix': 'data/FASTA/validation_',
+        'CSV_prefix': 'data/CSV/validation_'
     },
     'test': {
         'X' : [],
         'Y' : [],
-        'prefix': 'testing_'
+        'FASTA_prefix': 'data/FASTA/testing_',
+        'CSV_prefix': 'data/CSV/testing_'
     }
 }
 
+# Removing duplicate sequences in the dataset
+seen = set()
 for i in range(len(temperature_labels_full)):
     records = [] 
     # Parsing sequences (X dataset) from one dataset 
-    for record in SeqIO.parse(sys.argv[i+1], "fasta"):
+    for record in SeqIO.parse(files[i], "fasta"):
         records.append(record)
 
     # Creating Y dataset from temperature labels 
@@ -48,3 +72,7 @@ for i in range(len(temperature_labels_full)):
 print(len(data['train']['X']))
 print(len(data['validate']['X']))
 print(len(data['test']['X']))
+
+# Shuffling the datasets
+for element in data.keys():
+    data[element]['X'], data[element]['Y'] = shuffle(data[element]['X'], data[element]['Y'], random_state=1)
