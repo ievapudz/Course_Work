04865bf412e44963ba88463f7ecb09d47a6d30c4
ievapudz <pudziuvelyteieva@gmail.com>
2021-11-27 18:12:08 +0200

    Changed filter_sequences function - now it differs from the one that is present in ipynb notebook.

--- scripts/filter_sequences.py
+++ scripts/filter_sequences.py
@@ -1,47 +1,29 @@
+import os
 """
 This module contains a function that filters sequences based on whether they have
-embeddings or not. The lists of sequences with embeddings for each dataset are placed
-in ./data/EMB_ESM1b directory with a file extension `.lst`.
+embeddings or not. 
 """
 
-def filter_sequences_with_embeddings(data):
-    data_filtered = {
-        'train': {
-            'X': [],
-            'Y': [],
-            'FASTA_prefix': 'data/FASTA/filtered_training_',
-            'CSV_prefix': 'data/CSV/filtered_training_',
-            'embedding_list': './data/EMB_ESM1b/training_embeddings_sample.lst',
-            'embeddings': 'training_embeddings_sample'
-        },
-        'validate': {
-            'X': [],
-            'Y': [],
-            'FASTA_prefix': 'data/FASTA/filtered_validation_',
-            'CSV_prefix': 'data/CSV/filtered_validation_',
-            'embedding_list': './data/EMB_ESM1b/validation_embeddings.lst',
-            'embeddings': '/content/drive/MyDrive/validation_sequences'
-        },
-        'test': {
-            'X' : [],
-            'Y' : [],
-            'FASTA_prefix': 'data/FASTA/filtered_testing_',
-            'CSV_prefix': 'data/CSV/filtered_testing_',
-            'embedding_list': './data/EMB_ESM1b/testing_embeddings.lst',
-            'embeddings': '/content/drive/MyDrive/testing_sequences'
-        }
-    }
+def filter_sequences(data, key, path_to_embeddings):
+    # The function that replaces the filter_sequences_with_embeddings.
+    # The functionality stays the same, however new dictionary 'data' is not created.
+    embeddings_list = "embeddings_files.tmp"
+    command = "ls -1 "+path_to_embeddings+" > "+embeddings_list
+    os.system(command)
 
-    for element in data.keys():
-        emb_list_handle = open(data_filtered[element]['embedding_list'], 'r')
-        emb_list = emb_list_handle.readlines()
-        emb_list_handle.close()
-        emb_set = set()
-        for j in range(len(emb_list)):
-            emb_set.add(emb_list[j].split('.')[0])
-        for i in range(len(data[element]['X'])):
-            if(data[element]['X'][i].id.split('|')[1] in emb_set):
-                data_filtered[element]['X'].append(data[element]['X'][i])
-                data_filtered[element]['Y'].append(data[element]['Y'][i])
+    emb_list_handle = open(embeddings_list, 'r')
+    emb_list = emb_list_handle.readlines()
+    emb_list_handle.close()
+    emb_set = set()
+    for j in range(len(emb_list)):
+        emb_set.add(emb_list[j].split('.')[0])
 
-    return data_filtered
+    data[key]['X_filtered'] = []
+    data[key]['Y_filtered'] = []    
+    for i in range(len(data[key]['X'])):
+        if(data[key]['X'][i].id.split('|')[1] in emb_set):
+            data[key]['X_filtered'].append(data[key]['X'][i])
+            data[key]['Y_filtered'].append(data[key]['Y'][i])
+
+    command = "rm *.tmp"
+    os.system(command)
