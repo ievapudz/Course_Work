7b93712abc911ac09c6625db863350090dca6cd1
ievapudz <pudziuvelyteieva@gmail.com>
2022-01-08 05:41:48 +0200

    Edited comment and added the possibility to set the index, from which the download should begin.

--- scripts/data_download/get_UniProt_proteomes.sh
+++ scripts/data_download/get_UniProt_proteomes.sh
@@ -4,14 +4,15 @@ set -ue
 
 # A script to get proteomes based on UniParc identifier
 # Example usage:
-# ./scripts/data_download/get_UniProt_proteomes.sh data/003/proteome_UniParc_IDs_non_redundant_no_excluded.tsv
+# ./scripts/data_download/get_UniProt_proteomes.sh data/003/003.tsv 0
 
 DATASET_FILE=$1
+INITIAL_INDEX=$2
 
 NAMES=($(cat ${DATASET_FILE} | awk '{ print $1 }'))
 UP_IDS=($(cat ${DATASET_FILE} | awk '{ print $2 }'))
 
-for((i = 0 ; i < "${#UP_IDS[@]}" ; i++));
+for((i = "${INITIAL_INDEX}" ; i < "${#UP_IDS[@]}" ; i++));
 do
     echo "$i/${#UP_IDS[@]}: downloading ${UP_IDS[$i]}"
     PROTEOME_URL="https://www.uniprot.org/uniprot/?query=proteome:"${UP_IDS[$i]}"&format=fasta"
