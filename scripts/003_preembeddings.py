826dcb8e4684ed55aaac23086708ae6909c3b1d2
ievapudz <pudziuvelyteieva@gmail.com>
2022-01-12 09:23:26 +0200

    Added preprocessing scripts before embeddings generation.

--- scripts/003_preembeddings.py
+++ scripts/003_preembeddings.py
@@ -5,48 +5,41 @@ from Bio import SeqIO
 import math
 import random
 
-# The function that reads a list of files
-def read_file_list(list_file):
-    file_handle = open(list_file, 'r')
-    file_list = file_handle.readlines()
-    file_handle.close()
-    return file_list
+# This is a script that divides datasets into training, validation and testing subsets.
 
-# The function that based on the list of files, which names are in format [temperature_label]_[TaxID]_[domain].fasta,
-# parses each of the listed FASTA file and classifies its records to classes
-def classify_records(file_list, file_prefix, partition=65):
-    class_0 = []
-    class_1 = []
-    for file in file_list:
-        [temperature_label, tax_id, domain] = file.split('_')
-        for record in SeqIO.parse(file_prefix+file.rstrip(), 'fasta'):
-            record.name = tax_id+'|'+record.id.split('|')[1]+'|'+temperature_label
-            if(int(temperature_label) >= partition):
-                class_1.append(record)
-            else:
-                class_0.append(record)
-    class_0 = random.shuffle(class_0)
-    class_1 = random.shuffle(class_1)
-    return [class_0, class_1]
+# The function that reads FASTA file and saves records as Seq objects
+def read_FASTA(file_name):
+    FASTA_records = []
+    for record in SeqIO.parse(file_name, 'fasta'):
+        FASTA_records.append(record)
+    random.shuffle(FASTA_records)
+    return FASTA_records
     
 # The function that writes the needed fraction of an array to the subset file
 def write_FASTA(file_name, records_0, records_1, fraction, offset_0, offset_1):
     file_handle = open(file_name, 'w')
     number_of_records_0 = math.floor(len(records_0)*fraction)
     for i in range(offset_0, offset_0+number_of_records_0):
-        file_handle.write(records_0[i])
+        file_handle.write('>'+records_0[i].name+"\n")
+        file_handle.write(str(records_0[i].seq)+"\n")
+    
     number_of_records_1 = math.floor(len(records_1)*fraction)
     for i in range(offset_1, offset_1+number_of_records_1):
-        file_handle.write(records_1[i])
+        file_handle.write('>'+records_1[i].name+"\n")
+        file_handle.write(str(records_1[i].seq)+"\n")
     file_handle.close()
 
-FASTA_files_prefix = 'data/'
+FASTA_files_prefix = sys.argv[1]
 
-file_list = read_file_list(sys.argv[1])
-[class_0, class_1] = classify_records(file_list, sys.argv[2])
-write_FASTA(sys.argv[2]+'/training.fasta', class_0, class_1, 0.7, 0, 0)
-write_FASTA(sys.argv[2]+'/validation.fasta', class_0, class_1, 0.15, math.floor(len(class_0)*0.7), math.floor(len(class_1)*0.7))
-write_FASTA(sys.argv[2]+'/testing.fasta', class_0, class_1, 0.15, math.floor(len(class_0)*0.85), math.floor(len(class_1)*0.85))
+class_0 = read_FASTA(FASTA_files_prefix+'/class_0.fasta')
+class_1 = read_FASTA(FASTA_files_prefix+'/class_1.fasta')
+
+print("Writing training set")
+write_FASTA(FASTA_files_prefix+'/training.fasta', class_0, class_1, 0.7, 0, 0)
+print("Writing validation set")
+write_FASTA(FASTA_files_prefix+'/validation.fasta', class_0, class_1, 0.15, math.floor(len(class_0)*0.7), math.floor(len(class_1)*0.7))
+print("Writing testing set")
+write_FASTA(FASTA_files_prefix+'/testing.fasta', class_0, class_1, 0.15, math.floor(len(class_0)*0.85), math.floor(len(class_1)*0.85))
 
 
 
