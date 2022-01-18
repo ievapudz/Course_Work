10cc95592456a5a5d4f8d7d224874c1ec7e3ebad
ievapudz <pudziuvelyteieva@gmail.com>
2022-01-18 15:03:43 +0200

    Added SBATCH scripts to generate embeddings.

--- scripts/003_embeddings_validation_v2.sh
+++ scripts/003_embeddings_validation_v2.sh
@@ -5,22 +5,20 @@
 
 set -ue
 
-#SBATCH --job-name=003_training_v2
-#SBATCH --output=data/003/slurm/training.out
+#SBATCH --job-name=003_validation_v2
+#SBATCH --output=data/003/slurm/validation.out
 
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=2
 #SBATCH --mem-per-cpu=8000
-
-#SBATCH --array=3-4
+#SBATCH --array=1-2
 
 DATA_DIR=data/003/
-JOB_NAME=training_v2
+JOB_NAME=validation_v2
 FASTA_PREFIX=${DATA_DIR}FASTA/${JOB_NAME}/${JOB_NAME}.part-
 
 echo "Job started at $(date)"
 
-#for((i = 2 ; i < 4 ; i++));
+#for((i = 1 ; i < 8 ; i++));
 #do
 srun --ntasks 1 python3.7 ../programs/esm-0.4.0/extract.py esm1b_t33_650M_UR50S ${FASTA_PREFIX}${SLURM_ARRAY_TASK_ID}.fasta ${DATA_DIR}EMB_ESM1b/${JOB_NAME}/ --repr_layers 0 32 33 --include mean 
 #done
