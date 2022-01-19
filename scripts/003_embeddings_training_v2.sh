e5dd312290c8840113edfba1fb7fc204c23370bb
ievapudz <pudziuvelyteieva@gmail.com>
2022-01-19 12:50:17 +0200

    Added modified script to train v2 embeddings of 003 iteration.

--- scripts/003_embeddings_training_v2.sh
+++ scripts/003_embeddings_training_v2.sh
@@ -8,11 +8,9 @@ set -ue
 #SBATCH --job-name=003_training_v2
 #SBATCH --output=data/003/slurm/training.out
 
-#SBATCH --nodes=2
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=8000
-
-#SBATCH --array=3-4
 
 DATA_DIR=data/003/
 JOB_NAME=training_v2
@@ -20,10 +18,7 @@ FASTA_PREFIX=${DATA_DIR}FASTA/${JOB_NAME}/${JOB_NAME}.part-
 
 echo "Job started at $(date)"
 
-#for((i = 2 ; i < 4 ; i++));
-#do
 srun --ntasks 1 python3.7 ../programs/esm-0.4.0/extract.py esm1b_t33_650M_UR50S ${FASTA_PREFIX}${SLURM_ARRAY_TASK_ID}.fasta ${DATA_DIR}EMB_ESM1b/${JOB_NAME}/ --repr_layers 0 32 33 --include mean 
-#done
 
 echo "Job end at $(date)"
 
