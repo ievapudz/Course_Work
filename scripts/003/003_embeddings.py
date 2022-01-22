c6ad9ec3848ce534682bae1f9e54d3bbcbc26313
ievapudz <pudziuvelyteieva@gmail.com>
2022-01-22 08:49:39 +0200

    Added validation curves after NPZ file generation.

--- scripts/003/003_embeddings.py
+++ scripts/003/003_embeddings.py
@@ -15,22 +15,29 @@ from dataset_processing import create_data
 from dataset_processing import filter_sequences
 from dataset_processing import get_ESM_embeddings_as_tensor
 
+print("Creating data object")
 data = create_data('data/003/', dataset_names=['training_v2', 
                                                'validation_v2',
                                                'testing_v2'])
 
+print("Filtering training data")
 filter_sequences(data, 'train', data['train']['embeddings'])
-filter_sequences(data, 'validate', data['validate']['embeddings'])
-filter_sequences(data, 'test', data['test']['embeddings'])
 
+print("Filtering validation data")
+filter_sequences(data, 'validate', data['validate']['embeddings'])
+#filter_sequences(data, 'test', data['test']['embeddings'])
+
+print("Converting ESM embeddings (training) to tensor")
 [Xs_train_tensor, Ys_train_tensor] = get_ESM_embeddings_as_tensor(data, 
                                                                   ['train'])
+print("Converting ESM embeddings (validation) to tensor")
 [Xs_validate_tensor, Ys_validate_tensor] = get_ESM_embeddings_as_tensor(data, 
                                                                   ['validate'])
-[Xs_test_tensor, Ys_test_tensor] = get_ESM_embeddings_as_tensor(data, ['test'])
+#[Xs_test_tensor, Ys_test_tensor] = get_ESM_embeddings_as_tensor(data, ['test'])
 
+print("Saving tensors to NPZ file")
 save_tensors_as_NPZ([Xs_train_tensor, Ys_train_tensor, 
                      Xs_validate_tensor, Ys_validate_tensor], 
                     ['x_train', 'y_train', 'x_validate', 'y_validate'], 
-                    'data/003/NPZ/training_and_validation_embeddings_sample.npz')
+                    'data/003/NPZ/training_and_validation_embeddings_v2.npz')
 
