9396d4ceea0989baa4a60961e6e08e77bc79c3b3
ievapudz <pudziuvelyteieva@gmail.com>
2022-01-27 09:53:25 +0200

    Added MDE and MDE PCA visualisations of 003 dataset.

---
+++ scripts/003/003_visualisation.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3.7
+
+# A script that visualises the 003 datasets (separately)
+
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from Bio import SeqIO
+from dataset_processing import filter_sequences
+from visualise_embeddings import visualise_multiple_MDE_PCA
+from visualise_embeddings import visualise_multiple_MDE
+from file_actions import parse_dataset
+from file_actions import generate_embeddings
+from matplotlib.colors import ListedColormap
+
+two_color_cmap = ListedColormap(["navy", "red"])
+
+print("Creating data object")
+data = {
+    '003_train_v2': {
+        'X': [],
+        'Y': [],
+        'FASTA': 'data/003/FASTA/training_v2/training_v2.fasta',
+        'embeddings': 'data/003/EMB_ESM1b/training_v2'
+    },
+    '003_validate_v2': {
+        'X': [],
+        'Y': [],
+        'FASTA': 'data/003/FASTA/validation_v2/validation_v2.fasta',
+        'embeddings': 'data/003/EMB_ESM1b/validation_v2'
+    },
+    '003_test_v2': {
+        'X': [],
+        'Y': [],
+        'FASTA': 'data/003/FASTA/testing_v2/testing_v2.fasta',
+        'embeddings': 'data/003/EMB_ESM1b/testing_v2'
+    }
+}
+
+visualisation_file_path = 'data/003/visualisation_v2/'
+
+keys = ['003_train_v2', '003_test_v2']
+for key in keys:
+    print("Parsing dataset: "+key)
+    parse_dataset(data, key, 2)
+    print("Filtering sequences: "+key)
+    filter_sequences(data, key, data[key]['embeddings'])
+    print("Visualising MDE PCA: "+key)
+    visualise_multiple_MDE_PCA(data, [key], visualisation_file_path+key+"_MDE_PCA.png", two_color_cmap, False)
+    print("Visualising MDE: "+key)
+    visualise_multiple_MDE(data, [key], visualisation_file_path+key+"_MDE.png", two_color_cmap, False)
