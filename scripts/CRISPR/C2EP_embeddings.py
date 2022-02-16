d31dda81989f69ec55ddeafef003271bc0081f26
ievapudz <pudziuvelyteieva@gmail.com>
2022-02-16 08:41:13 +0200

    Created CSV and TSV files to represent sequences and their embeddings.

--- scripts/CRISPR/C2EP_embeddings.py
+++ scripts/CRISPR/C2EP_embeddings.py
@@ -4,6 +4,9 @@
 # NPZ file.
 # It has to be run after embeddings were generated.
 
+# Usage:
+# ./scripts/CRISPR/C2EP_embeddings.py > data/CRISPR/C2EP_embeddings.csv
+
 import sys
 import os
 
@@ -11,22 +14,23 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from file_actions import generate_embeddings
+from file_actions import print_tensor_as_CSV
 from file_actions import save_tensors_as_NPZ
 from dataset_processing import create_testing_data
 from dataset_processing import filter_sequences
 from dataset_processing import get_ESM_embeddings_as_tensor
 
-print("Creating data object")
-data = create_testing_data('data/CRISPR/', dataset_names=['C2EP'])
+data = create_testing_data('data/CRISPR/', dataset_names=['C2EP'], 
+                           labelled=False)
 
-print("Filtering training data")
-filter_sequences(data, 'test', data['test']['embeddings'])
+filter_sequences(data, 'test', data['test']['embeddings'], labelled=False)
 
-print("Converting ESM embeddings to tensor")
-[Xs_test_tensor, Ys_test_tensor] = get_ESM_embeddings_as_tensor(data, ['test'])
+[Xs_test_tensor, Ys_test_tensor] = get_ESM_embeddings_as_tensor(data, ['test'],
+                                   labelled=False)
 
-print("Saving tensors to NPZ file")
-save_tensors_as_NPZ([Xs_test_tensor, Ys_test_tensor], 
-                    ['x_test', 'y_test'], 
+save_tensors_as_NPZ([Xs_test_tensor, Ys_test_tensor], ['x_test', 'y_test'], 
                     'data/CRISPR/NPZ/C2EP_embeddings.npz')
+
+data_tensor = { 'x_test': Xs_test_tensor, 'y_test': Ys_test_tensor }
+
+print_tensor_as_CSV(data, data_tensor, 'test', ['x_test', 'y_test'], labelled=False)
