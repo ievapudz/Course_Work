8903e6ad6b86fd79877e4993a661fb97cd2151f6
ievapudz <pudziuvelyteieva@gmail.com>
2022-02-23 07:38:39 +0200

    Changes scale of the axes.

--- scripts/003/003_plot_correlation.py
+++ scripts/003/003_plot_correlation.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import numpy as np
 
 df = pd.read_csv(sys.argv[1], sep="\t")
 
@@ -11,6 +12,8 @@ y = df['prediction']
 
 plt.scatter(y, x)
 plt.title('A plot to show the correlation between temperature and prediction')
+plt.yticks(np.arange(-2.5, 2.5, 0.5))
+plt.xticks(np.arange(-2.5, 2.5, 0.5))
 plt.xlabel('Normalised temperature')
 plt.ylabel('Prediction value')
 plt.plot()
