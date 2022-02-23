1d90de6a8b52b144ee953351361a13e3b2d45170
ievapudz <pudziuvelyteieva@gmail.com>
2022-02-23 08:01:58 +0200

    Changed scatterplot colour to navy blue by argument c.

--- scripts/003/003_plot_correlation.py
+++ scripts/003/003_plot_correlation.py
@@ -10,13 +10,13 @@ df = pd.read_csv(sys.argv[1], sep="\t")
 x = df['temperature']
 y = df['prediction']
 
-plt.scatter(y, x)
+plt.scatter(y, x, c='navy')
 plt.title('A plot to show the correlation between temperature and prediction')
 plt.ylim(-2.5, 2.5)
 plt.xlim(-2.5, 2.5) 
 plt.xlabel('Normalised temperature')
 plt.ylabel('Prediction value')
-plt.plot(color='navy')
+plt.plot()
 
 m, b = numpy.polyfit(x, y, 1)
 plt.plot(x, m*x+b, color='lightpink')
