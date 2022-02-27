3bebf4ceb9434d05607707eebcaab41b8ddd6182
ievapudz <pudziuvelyteieva@gmail.com>
2022-02-27 15:42:00 +0200

    Added workflow files for accuracy calculation (script and results).

--- scripts/003/003_accuracy_per_organism.py
+++ scripts/003/003_accuracy_per_organism.py
@@ -7,7 +7,7 @@
 # Output: percentage of correctly predicted temperature classes.
 
 # Call:
-# ./scripts/003/003_accuracy_per_organism.py results/SLP/003/predictions.tsv
+# ./scripts/003/003_accuracy_per_organism.py results/SLP/003/testing_v2_predictions.tsv results/SLP/003/testing_v2_accuracy_per_taxid.tsv
 
 import sys
 import os
@@ -20,9 +20,10 @@ from file_actions import return_SV_as_dict
 from model_flow import calculate_accuracy_per_tax_id
 
 predictions_file_name = sys.argv[1]
+out_file_name = sys.argv[2]
 
-SV_contents =  return_SV_as_dict(predictions_file_name, 0, 3, 1285, 
+SV_contents =  return_SV_as_dict(predictions_file_name, 0, 2, 3, 
                                  separator="\t", has_header=False,
                                  threshold=0.5)
 
-calculate_accuracy_per_tax_id(SV_contents, 'results/SLP/003/testing_accuracy_per_taxid.tsv')
+calculate_accuracy_per_tax_id(SV_contents, out_file_name)
