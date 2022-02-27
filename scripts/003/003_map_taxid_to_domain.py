e97e392dfadd4c2dff9a91792fb7f762d9585e33
ievapudz <pudziuvelyteieva@gmail.com>
2022-02-27 16:07:06 +0200

    Added the results of accuracy calculation with organism domains mapped.

--- scripts/003/003_map_taxid_to_domain.py
+++ scripts/003/003_map_taxid_to_domain.py
@@ -5,6 +5,7 @@
 
 # Example usage:
 # ./scripts/003/003_map_taxid_to_domain.py ./data/003/TSV/all_taxids_domains.tsv ./data/003/TSV/validation_v2_tensors.tsv
+# ./scripts/003/003_map_taxid_to_domain.py ./data/003/TSV/all_taxids_domains.tsv ./results/SLP/003/testing_v2_accuracy_per_taxid.tsv
 
 import sys
 import os
@@ -21,7 +22,7 @@ dataset_TSV_file = sys.argv[2]
 
 mapping = read_map_from_SV(taxid_domain_map_file)
 
-values = get_values_from_SV(dataset_TSV_file, [0, 1])
+values = get_values_from_SV(dataset_TSV_file, [0, 1, 2, 3], headerless=False)
 
 for index, value_pair in enumerate(values):
     print("\t".join(value_pair), "\t", mapping[value_pair[0]])
