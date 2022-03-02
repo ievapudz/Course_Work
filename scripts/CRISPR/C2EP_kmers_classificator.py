4ed1dff824bf31f3bc9a4789f4591ee4334366c4
ievapudz <pudziuvelyteieva@gmail.com>
2022-03-02 10:10:22 +0200

    Added inference making flow with kmers.

---
+++ scripts/CRISPR/C2EP_kmers_classificator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.7
+
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import torch
+from torch import nn
+from model_dataset_processing import load_tensor_from_NPZ
+from SLP import SLP_with_sigmoid
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+from model_flow import unlabelled_test_epoch
+
+PATH = './results/SLP/003/model.pt'
+
+dataset = load_tensor_from_NPZ('data/CRISPR/NPZ/C2EP_kmers_embeddings.npz', 
+                               ['x_test', 'y_test'])
+
+test_dataset = TensorDataset(dataset['x_test'], dataset['y_test'])  
+test_loader = DataLoader(test_dataset, shuffle=False)
+
+# Set fixed random number seed
+torch.manual_seed(42)
+
+# Load the trained SLP
+model = SLP_with_sigmoid()
+model.load_state_dict(torch.load(PATH))
+model.eval()
+
+print('Inference process begins')
+
+unlabelled_test_epoch(model, test_loader, 0.5, 
+                      file_for_predictions='results/SLP/CRISPR/C2EP_kmers_predictions.tsv',
+                      binary_predictions_only=False)
+
+print('Inference process has finished.')
+
