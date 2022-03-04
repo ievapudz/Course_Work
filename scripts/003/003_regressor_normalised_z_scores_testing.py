7dd570f8ab2a2e5a88bc8588e1c1c6c827265b6f
ievapudz <pudziuvelyteieva@gmail.com>
2022-03-04 09:34:03 +0200

    Added calculation of MCC for regressor output (with different thresholds), edited the testing epoch functions (removed abundant arguments), rerun scripts for regression for normalised values as z scores and by division of max temperature, ran the flow for normalised data by division of 100.

--- scripts/003/003_regressor_normalised_z_scores_testing.py
+++ scripts/003/003_regressor_normalised_z_scores_testing.py
@@ -69,8 +69,7 @@ test_dataset = TensorDataset(dataset_test_initial['x_test'],
                              dataset_test_initial['y_test'])
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-test_epoch(regressor, test_loader, loss_function, optimizer, BATCH_SIZE,
-           EPOCH_BATCH_SIZE, 
+test_epoch(regressor, test_loader, BATCH_SIZE,
            ROC_curve_plot_file_dir='',
            confusion_matrix_file_dir='', 
            file_for_predictions='results/regressor/003/testing_4_real_and_predictions_z_scores.tsv')
