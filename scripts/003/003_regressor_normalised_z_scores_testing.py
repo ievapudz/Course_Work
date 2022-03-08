7226b311adf9dfbd14888f57ac11ea433d1fb1ff
ievapudz <pudziuvelyteieva@gmail.com>
2022-03-08 06:51:16 +0200

    Added printing of the header for the output TSV file (with predictions).

--- scripts/003/003_regressor_normalised_z_scores_testing.py
+++ scripts/003/003_regressor_normalised_z_scores_testing.py
@@ -69,9 +69,15 @@ test_dataset = TensorDataset(dataset_test_initial['x_test'],
                              dataset_test_initial['y_test'])
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
+predictions_out_file = 'results/regressor/003/testing_4_real_and_predictions_z_scores.tsv'
+
+command = 'echo "temperature\tprediction" >'+predictions_out_file
+
+os.system(command)
+
 test_epoch(regressor, test_loader, BATCH_SIZE,
            ROC_curve_plot_file_dir='',
            confusion_matrix_file_dir='', 
-           file_for_predictions='results/regressor/003/testing_4_real_and_predictions_z_scores.tsv')
+           file_for_predictions=predictions_out_file)
 
 
