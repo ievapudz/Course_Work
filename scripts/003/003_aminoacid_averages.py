1859a5779804d67bb2f5775132cb152a9c2099c7
ievapudz <pudziuvelyteieva@gmail.com>
2022-03-12 20:43:09 +0200

    Added reference line from numpy array.

--- scripts/003/003_aminoacid_averages.py
+++ scripts/003/003_aminoacid_averages.py
@@ -46,6 +46,6 @@ max_val = max(class_means_df[0])
 if max(class_means_df[1]) > max_val:
     max_val = max(class_means_df[1])
 
-plt.plot(numpy.array[0, max_val])
+plt.plot(numpy.array([0, max_val]), numpy.array([0, max_val]))
 
 plt.savefig(sys.argv[2])
