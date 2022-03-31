204d6903ce29024d9c523c98cd914ec321636a36
ievapudz <pudziuvelyteieva@gmail.com>
2022-03-31 10:28:08 +0300

    Added script to create datasets for the model for each temperature range. FASTA files were created, but not included in the repository.

--- scripts/004/004_construct_datasets.py
+++ scripts/004/004_construct_datasets.py
@@ -17,31 +17,36 @@ from dataset_processing import get_list_of_proteomes
 from dataset_processing import filter_sequences_by_length_before_embeddings
 from file_actions import print_SeqRecords_to_FASTA 
 
-directory = sys.argv[1]
+proteome_directory = sys.argv[1]
+sets_directory = sys.argv[2]
+
 seqs_in_set = 10000
 
-ranges = [ '^[0-9]_.*' ]#, 
-		   #'^1[0-4]_.*', '^1[5-9]_.*', 
-		   #'^2[0-4]_.*', '^2[5-9]_.*', 
-		   #'^3[0-4]_.*', '^3[5-9]_.*',
-		   #'^4[0-4]_.*', '^4[5-9]_.*', 
-		   #'^5[0-4]_.*', '^5[5-9]_.*',
-		   #'^6[0-4]_.*', '^6[5-9]_.*', 
-		   #'^7[0-4]_.*', '^7[5-9]_.*',
-		   #'^8[0-4]_.*', '^8[5-9]_.*', 
-		   #'^9[0-9]_.*' ]
+ranges = [ '^([0-9]_.*|(1[0-4])_.*)_.*$', 
+		   '^1[5-9]_.*', 
+		   '^2[0-4]_.*', '^2[5-9]_.*', 
+		   '^3[0-4]_.*', '^3[5-9]_.*',
+		   '^4[0-4]_.*', '^4[5-9]_.*', 
+		   '^5[0-4]_.*', '^5[5-9]_.*',
+		   '^6[0-4]_.*', '^6[5-9]_.*', 
+		   '^7[0-4]_.*', '^7[5-9]_.*',
+		   '^8[0-4]_.*', 
+		   '^(8[5-9]_.*|(9[0-9])_.*|100_.*)_.*$' ]
 
 max_seq_in_prots = 800
 
 sets = {}
 
 for range_regex in ranges:
-	proteomes = get_list_of_proteomes(directory, range_regex)
-	filtered_sequences = filter_sequences_by_length_before_embeddings(proteomes, directory, 1022)
+	proteomes = get_list_of_proteomes(proteome_directory, range_regex)
+	filtered_sequences = filter_sequences_by_length_before_embeddings(proteomes, proteome_directory, 1022)
 	
 	one_range_sets = fill_model_sets(proteomes, filtered_sequences, range_regex, max_seq_in_prots, seqs_in_set,
 					[0.7, 0.15, 0.15], 1022)
 
 	sets[range_regex] = one_range_sets
 
-print_SeqRecords_to_FASTA(sets['^[0-9]_.*']['train'], 'train_tmp.fasta')
+for i, range_regex in enumerate(ranges):
+	for set_name in ['train', 'validate', 'test']:
+		print_SeqRecords_to_FASTA(sets[ranges[i]][set_name], sets_directory+'/'+\
+								  set_name+'_'+str(i)+'.fasta')
