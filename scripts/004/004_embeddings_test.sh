2edf90016347115ee3e73ef78264f8a75b3a5ba5
ievapudz <pudziuvelyteieva@gmail.com>
2022-04-05 16:43:28 +0300

    Added scripts that were used to get 004 dataset embeddings.

---
+++ scripts/004/004_embeddings_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# It is a submission script to run embeddings
+# generation program.
+
+set -ue
+
+#SBATCH --job-name=004_test
+#SBATCH --output=data/004/slurm/test.out
+
+#SBATCH --nodes=7
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8000
+
+DATA_DIR=data/004/
+JOB_NAME=test
+FASTA_PREFIX=${DATA_DIR}FASTA/${JOB_NAME}/${JOB_NAME}_
+
+echo "Job started at $(date)"
+
+srun --ntasks 1 python3.7 ../programs/esm-0.4.0/extract.py esm1b_t33_650M_UR50S\
+	${FASTA_PREFIX}${SLURM_ARRAY_TASK_ID}.fasta ${DATA_DIR}EMB_ESM1b/${JOB_NAME}/\
+	 --repr_layers 33 --include mean 
+
+echo "Job end at $(date)"
+
