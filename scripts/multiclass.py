14d8bf91e447ed9f1824d0250574e7646e18e9af
ievapudz <pudziuvelyteieva@gmail.com>
2022-04-24 19:57:11 +0300

    Added flow for multiclass classifier with one hidden layer (MultiClass2).

--- scripts/multiclass.py
+++ scripts/multiclass.py
@@ -14,3 +14,18 @@ class MultiClass1(nn.Module):
 	def forward(self, x):
 		return self.layers(x)
 
+class MultiClass2(nn.Module):
+
+	# A model for multi-class (16) classification
+
+	def __init__(self):
+		super().__init__()
+		self.layers = nn.Sequential(
+			nn.Linear(1280, 640),
+			nn.ReLU(),
+			nn.Linear(640, 16),
+			nn.Softmax(dim=1)
+		)
+
+	def forward(self, x):
+		return self.layers(x)
