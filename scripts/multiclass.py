19b91c310e319bc95ca138d34fe47ec04a5535f3
ievapudz <pudziuvelyteieva@gmail.com>
2022-04-25 15:56:37 +0300

    Added architecture with 2 hidden layers.

--- scripts/multiclass.py
+++ scripts/multiclass.py
@@ -29,3 +29,21 @@ class MultiClass2(nn.Module):
 
 	def forward(self, x):
 		return self.layers(x)
+
+class MultiClass3(nn.Module):
+
+	# A model for multi-class (16) classification
+
+	def __init__(self):
+		super().__init__()
+		self.layers = nn.Sequential(
+			nn.Linear(1280, 640),
+			nn.ReLU(),
+			nn.Linear(640, 320),
+			nn.ReLU(),
+			nn.Linear(320, 16),
+			nn.Softmax(dim=1)
+		)
+
+	def forward(self, x):
+		return self.layers(x)
