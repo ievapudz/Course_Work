36fb52eeb367d504ad3332695cd2eba468ce0163
ievapudz <pudziuvelyteieva@gmail.com>
2022-04-29 15:12:59 +0300

    Added merged tables for Cas12a and Cas12b domain predictions (mean and per token averaged).

--- thermoclass/sbatch_thermoclass.sh
+++ thermoclass/sbatch_thermoclass.sh
@@ -13,12 +13,16 @@ set -ue
 #SBATCH --mem-per-cpu=8000
 
 DIR=FASTA/
-FASTA_PREFIX=C2EP.part-
+FASTA_PREFIX=Cas12b_C
 
-FASTA="${DIR}/${FASTA_PREFIX}${SLURM_ARRAY_TASK_ID}.fasta"
-srun ./thermoclass -f "${FASTA}" -c -p -e "emb/C2EP/" -t "emb/C2EP_per_tok_${SLURM_ARRAY_TASK_ID}.tsv" -n \
-               "emb/C2EP_per_tok_${SLURM_ARRAY_TASK_ID}.npz" -o "predictions/TSV/C2EP_per_tok_${SLURM_ARRAY_TASK_ID}.tsv" \
-                --output_fasta "predictions/FASTA/C2EP_per_tok_${SLURM_ARRAY_TASK_ID}.fasta" --output_plot \
-               "predictions/PNG/"
+#FASTA="${DIR}/${FASTA_PREFIX}.part-${SLURM_ARRAY_TASK_ID}.fasta"
+#srun ./thermoclass -f "${FASTA}" -g --per_tok -e "emb/${FASTA_PREFIX}/" -t "emb/${FASTA_PREFIX}_per_tok_${SLURM_ARRAY_TASK_ID}.tsv" -n \
+#			   "emb/${FASTA_PREFIX}_per_tok_${SLURM_ARRAY_TASK_ID}.npz" -o "predictions/TSV/${FASTA_PREFIX}_per_tok_${SLURM_ARRAY_TASK_ID}.tsv" \
+#				--output_fasta "predictions/FASTA/${FASTA_PREFIX}_per_tok_${SLURM_ARRAY_TASK_ID}.fasta" --output_plot \
+#			   "predictions/PNG/"
 
-
+FASTA="${DIR}/${FASTA_PREFIX}.fasta"
+srun ./thermoclass -f "${FASTA}" -g --per_tok -e "emb/${FASTA_PREFIX}/" -t "emb/${FASTA_PREFIX}_per_tok.tsv" -n \
+			   "emb/${FASTA_PREFIX}_per_tok.npz" -o "predictions/TSV/${FASTA_PREFIX}_per_tok.tsv" \
+				--output_fasta "predictions/FASTA/${FASTA_PREFIX}_per_tok.fasta" --output_plot \
+			   "predictions/PNG/"
