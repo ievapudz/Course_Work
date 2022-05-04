fdac0be1fb1859c3397270a7d8befd52a4ef301c
ievapudz <pudziuvelyteieva@gmail.com>
2022-05-04 10:34:28 +0300

    Added regressor training, validation, and testing scripts. Additioanlly, added results analysis (correlation coefficients calculation and plotting).

--- scripts/regressor.py
+++ scripts/regressor.py
@@ -2,17 +2,17 @@ from torch import nn
 
 class Regressor(nn.Module):
 
-    # This is a regressor model to predict the
-    # temperature of the organism that the protein
-    # belongs to. 
-    # The input of the model is protein's embedding
-    # of 1280 dimensions.
+	# This is a regressor model to predict the
+	# temperature of the organism that the protein
+	# belongs to. 
+	# The input of the model is protein's embedding
+	# of 1280 dimensions.
 
-    def __init__(self):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(1280, 1),
-        )
+	def __init__(self):
+		super().__init__()
+		self.layers = nn.Sequential(
+			nn.Linear(1280, 1),
+		)
 
-    def forward(self, x):
-        return self.layers(x)
+	def forward(self, x):
+		return self.layers(x)
