713a74977e9bd59fa5432850f169187df5d15b46
ievapudz <pudziuvelyteieva@gmail.com>
2022-05-11 12:38:15 +0300

    Added the first plot for visualisation of analysis deltas.

---
+++ scripts/misc/make_dot_plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.7
+
+# A script that draws a plot for data dots
+# from a single file (TSV)
+
+import os
+import sys
+import matplotlib.pyplot as plt
+
+data_file = sys.argv[1]
+x_index = int(sys.argv[2])
+y_indeces = list(map(int,sys.argv[3:]))
+
+file_handle = open(data_file, "r")
+lines = file_handle.readlines()[1:]
+file_handle.close()
+
+x = []
+y = []
+for y_index in y_indeces:
+	for line in lines:
+		x.append(line.split(' ')[x_index])
+		y.append(line.split(' ')[y_index])
+
+	plt.scatter(x, y)
+	x = []
+	y = []
+
+pathname, extension = os.path.splitext(data_file)
+plt.savefig(pathname.split('/')[-1]+'.png')
+
