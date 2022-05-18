f7c0806c51214f7194eee094d87ce25260c0f2c5
ievapudz <pudziuvelyteieva@gmail.com>
2022-05-18 10:34:19 +0300

    Added N term overlap analysis (only mean-wise).

--- scripts/misc/overlap_per_tok_means.py
+++ scripts/misc/overlap_per_tok_means.py
@@ -4,9 +4,10 @@
 # and calculates means for each overlap.
 
 # Example usage:
-# ./scripts/misc/overlap_per_tok_means.py predictions/TSV/Cas12a_1022_splits.part-1.per_tok.tsv predictions/TSV/Cas12a_N_per_tok_1.tsv 4 4 
+# ./scripts/misc/overlap_per_tok_means.py predictions/TSV/Cas12a_1022_splits.part-1.per_tok.tsv predictions/TSV/Cas12a_N_per_tok_1.tsv 4 4 N 
 
 import sys
+from statistics import mean
 
 sequences = {}
 
@@ -14,6 +15,7 @@ file_1 = sys.argv[1]
 file_2 = sys.argv[2]
 index_1 = int(sys.argv[3])
 index_2 = int(sys.argv[4])
+term = sys.argv[5]
 
 file_handle = open(file_1, 'r')
 lines_1 = file_handle.readlines()[1:]
@@ -26,7 +28,8 @@ for line in lines_1:
 		sequences[seq_id] = {}
 		sequences[seq_id]['0'] = []
 		sequences[seq_id]['1'] = []
-	prediction = line.split('\t')[index_1]
+	prediction = float(line.split('\t')[index_1])
+	
 	sequences[seq_id]['0'].append(prediction)
 
 file_handle = open(file_2, 'r')
@@ -34,9 +37,24 @@ lines_2 = file_handle.readlines()[1:]
 file_handle.close()
 
 for line in lines_2:
-    line = line.rstrip()
-    seq_id = line.split('\t')[0]
-    prediction = line.split('\t')[index_2]
-    sequences[seq_id]['1'].append(prediction)
+	line = line.rstrip()
+	seq_id = line.split('\t')[0]
+	prediction = float(line.split('\t')[index_2])
+	sequences[seq_id]['1'].append(prediction)
 
-print(len(sequences['Adurb193_1']['0']))
+if(term == 'N'):
+	for seq_id in list(sequences.keys()):
+		overlap_length = min([len(sequences[seq_id]['0']), 
+								len(sequences[seq_id]['1'])])
+		if(overlap_length):
+			mean_1 = mean(sequences[seq_id]['0'][0:overlap_length])
+			mean_2 = mean(sequences[seq_id]['1'][0:overlap_length])
+			print(seq_id, '\t', mean_1, '\t', mean_2, '\t', mean_1-mean_2)
+elif(term == 'C'):
+	for seq_id in list(sequences.keys()):
+		overlap_length = min([len(sequences[seq_id]['0']), 
+								len(sequences[seq_id]['1'])])
+		if(overlap_length):
+			mean_1 = mean(sequences[seq_id]['0'][len(sequences[seq_id]['0'])-overlap_length:])
+			mean_2 = mean(sequences[seq_id]['1'][len(sequences[seq_id]['1'])-overlap_length:])
+			print(seq_id, '\t', mean_1, '\t', mean_2, '\t', mean_1-mean_2)
